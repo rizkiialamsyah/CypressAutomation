8df334f8a089e28359f24da87cb3384c3d18c98d
Rizki Alamsyah <rizkikenway@gmail.com>
2023-10-19 11:08:07 +0700

    Update beberapa latihan untuk table

--- cypress/e2e/examples/test6.js
+++ cypress/e2e/examples/test6.js
@@ -5,13 +5,30 @@ describe("My Fifth Test Suite handling Web Table", () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice");
     cy.get("tr td:nth-child(2)").each(($el, index, list) => {
       const textFinder = $el.text();
-      if (textFinder.includes("Practical")) {
+      if (textFinder.includes("TestNG")) {
         cy.get("tr td:nth-child(2)")
           .eq(index)
           .next()
           .then((price) => {
             const priceText = price.text();
-            expect(priceText).to.equal("25");
+            expect(priceText).to.equal("20");
+          });
+      }
+    });
+  });
+
+  it("Test Web Table Head Fixed", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice");
+    cy.get("tr td:nth-child(1)").each(($el, index, list) => {
+      const pencariTeks = $el.text();
+      if (pencariTeks.includes("Ivory")) {
+        cy.get("tr td:nth-child(1)")
+          .eq(index)
+          .next()
+          .then((jobTitle) => {
+            const jobDesc = jobTitle.text();
+            expect(jobDesc).to.equal("Receptionist");
+            cy.log(jobDesc);
           });
       }
     });
